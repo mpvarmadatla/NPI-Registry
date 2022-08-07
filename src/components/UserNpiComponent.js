2dbf42142e6eae495d5c326c0f880c8ac554f6ac
manoj pavan varma datla <mpvarmadatla@manojs-MacBook-Pro.local>
2022-08-07 10:51:05 +0530

    add validation activityindicator

--- src/components/UserNpiComponent.js
+++ src/components/UserNpiComponent.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import {Text,View} from 'react-native';
+import {ActivityIndicator, Text,View} from 'react-native';
 import ListItem from '../util/ListItem';
 
 const UserNpiComponent = ({route}) =>{
@@ -8,7 +8,7 @@ const UserNpiComponent = ({route}) =>{
     if(route.params?.lastname){
         fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.0&last_name=${route.params?.lastname}`)
         .then((res)=>res.json())
-        .then((json)=>setData(json))
+        .then((json)=>setData(json) )
         .catch((error)=>console.log(error))
     }
     
@@ -21,6 +21,7 @@ const UserNpiComponent = ({route}) =>{
           <Text style={{color:'#000',fontSize:16,fontWeight:'bold'}}>Name</Text>
           <Text style={{color:'#000',fontSize:16,fontWeight:'bold'}}>Phone</Text>
          </View>   
+            {!data && <ActivityIndicator size="large" color="#00ff00" />}
             {data && data?.results.map((item)=><ListItem item={item} />)}
         </View>
     )
